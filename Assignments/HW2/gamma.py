ca1a275c478122d8dd3054e4c3ac9aa3248a3883
rakshith <rvenkatachalapathy@dons.usfca.edu>
2020-02-20 02:40:41 -0800

    Importing intg

--- Assignments/HW2/gamma.py
+++ Assignments/HW2/gamma.py
@@ -1,3 +1,9 @@
+import sys
+
+sys.path.insert(0, '../HW1')
+from integral import intg
+
+
 def fact(n):
     if n < 0:
         return
@@ -10,30 +16,50 @@ def fact(n):
     return sum
 
 
-if __name__ == "__main__":
-    import doctest
-    import argparse
-    import math
-    import re
-
+def parse_agrs():
     parser = argparse.ArgumentParser()
     parser.add_argument('-x')
     # [-+]?[0-9]*\.+[0-9]*
     args = parser.parse_args()
 
-    x = args.x
-    m = re.match("[-+]?[0-9]*\.+[0-9]*", x)
-    if re.match("[-+]?[0-9]*\.+[0-9]*", x):
-        x = float(x)
+    t = args.x
+    m = re.match("[-+]?[0-9]*\.+[0-9]*", t)
+    if re.match("[-+]?[0-9]*\.+[0-9]*", t):
+        t = float(t)
 
     else:
-        x = int(x)
-    print(type(x))
-    result = 0
+        t = int(t)
+
+    return t
+
+
+def call_func(t):
+    if isinstance(t, int) and t >= 1:
+        result = fact(t - 1)
+    elif isinstance(t, float) and t >= 1:
+        expr = lambda x: (x ** (t - 1)) * math.exp(-1 * x)
+        result = intg(expr, 0, 1000, 1e-4, False)
+    else:
+        print("The value of 't' is less than 1")
+        result = 0
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    import argparse
+    import math
+    import re
+    import pdb
+
+    t = parse_agrs()
     # based on the type of x call two functions
-    if isinstance(x, int):
-        result = fact(x - 1)
-    else:
-        print("hi")
 
-    print(result)
+    result = call_func(t)
+
+    if isinstance(result, int):
+        print('The factorial of t-1 is {:1}'.format(result))
+    else:
+        print('The result of the integral is {:1.8f}'.format(result[0]))
+        print('The fract_diff is {:1.8f}'.format(result[1]))
