e9e21ebd7a23d86273ed05f0e11cf000d57164e1
Rakshith Venkatachalapathy <54309883+rakshithvenkatachalapathy@users.noreply.github.com>
2020-02-28 19:42:04 -0800

    two figure plot

--- Assignments/HW03/plot_antisym_rvenkatachalapathy.py
+++ Assignments/HW03/plot_antisym_rvenkatachalapathy.py
@@ -1,20 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import argparse
-import math
 import sys
 
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm, pyplot  # color map
+
+from matplotlib import rc
+
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+rc('text', usetex=False)
+rc('font', family='serif')
+
+txt = "\n\nThe \"effective\" interaction between two neutral Fermions (both spin up) that arises from the symmetry " \
+      "requirement on the total wave function. "
+
+
+def f2(x, y):
+    return np.sin(np.sqrt(np.power(x, 2) + np.power(y, 2)))
 
 
 def antisym(x1, x2, n1=1, n2=2, a=1.0):
-    print("ays")
-    pone = math.sin(n1 * math.pi * x1 / a) * math.sin(n2 * math.pi * x2 / a)
-    ptwo = math.sin(n1 * math.pi * x2 / a) * math.sin(n2 * math.pi * x1 / a)
+    pone = np.sin((n1 * np.pi * x1) / a) * np.sin((n2 * np.pi * x2) / a)
+    ptwo = np.sin((n1 * np.pi * x2) / a) * np.sin((n2 * np.pi * x1) / a)
     val = (2 / a) * (pone - ptwo)
     return val
 
@@ -26,8 +34,8 @@ def parse_agrs():
     """
     ret = ()
     parser = argparse.ArgumentParser()
-    parser.add_argument('-x1')
-    parser.add_argument('-x2')
+    parser.add_argument('-x1_steps')
+    parser.add_argument('-x2_steps')
     parser.add_argument('-n1')
     parser.add_argument('-n2')
     parser.add_argument('-a')
@@ -35,12 +43,15 @@ def parse_agrs():
     # Read the command line input using argparse
 
     if len(sys.argv) == 5:
-        x1 = int(args.x1)
-        x2 = int(args.x2)
-        ret = (x1, x2)
+        x1 = int(args.x1_steps)
+        x2 = int(args.x2_steps)
+        n1 = 1
+        n2 = 2
+        a = 1.0
+        ret = (x1, x2, n1, n2, a)
     elif len(sys.argv) == 11:
-        x1 = int(args.x1)
-        x2 = int(args.x2)
+        x1 = int(args.x1_steps)
+        x2 = int(args.x2_steps)
         n1 = int(args.n1)
         n2 = int(args.n2)
         a = float(args.a)
@@ -50,8 +61,29 @@ def parse_agrs():
 
 if __name__ == "__main__":
     arg = parse_agrs()
-    if len(arg) == 2:
-        print("2")
-    elif len(arg) == 5:
-        print("many")
+    Z = 0
+
+    fig = plt.figure(figsize=(9, 4))
+
+    ax1 = fig.add_subplot(121, projection='3d')
+    ax1.set_title('$Antisymmetric  Spatial  Wave  Function $')
+
+    # x = y = np.linspace(-5, 5, 50)
+    x = np.linspace(0, arg[4], arg[0])
+
+    y = np.linspace(0, arg[4], arg[1])
+    X, Y = np.meshgrid(x, y)
+    Z = antisym(X, Y)
+    Z2 = antisym(X, Y) ** 2
+
+    ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0)
+
+    ax2 = fig.add_subplot(122, projection='3d')
+    ax2.set_title('$  Probability Density$')
+
+    ax2.plot_surface(X, Y, Z2, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0)
+    fig.text(.5, .001, txt, ha='center')
+
+    plt.ion()
+    plt.show()
 
