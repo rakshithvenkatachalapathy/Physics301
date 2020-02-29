4ae3dca15df69b82f0c1ba684eb9ddef454f95d5
Rakshith Venkatachalapathy <54309883+rakshithvenkatachalapathy@users.noreply.github.com>
2020-02-28 21:32:11 -0800

    comments for prime

--- Assignments/HW03/prime_rvenkatachalapathy.py
+++ Assignments/HW03/prime_rvenkatachalapathy.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """
 HW03
 Rakshith Venkatachalapathy
+
+This module is for HW03 to calculate the primes between two numbers
+Please provide the first number with the -s identifier and the second number with the -e identifier
+
+>>> primes(1,3)
+[1 2 3]
+
+>>> primes(1,5)
+[1 2 3 5]
+
+>>> primes(1,11)
+[ 1  2  3  5  7 11]
+
+>>> isprime(11) == 11
+True
 """
 import argparse
 
@@ -14,22 +23,32 @@ import numpy as np
 
 
 def isprime(num):
+    """
+    Checks if a number is prime and returns a prime
+    :param num: number
+    :return: returns  a number if prime
+
+    >>> isprime(11) == 11
+    True
+    """
+
+    if num == 1:
+        return num
+
     if num > 1:
-        # Iterate from 2 to n / 2
-        for i in range(2, num // 2):
-            # If num is divisible by any number between   2 and n / 2, it is not prime
+        # Iterate from 2 to num
+        for i in range(2, num):
+            # If num is divisible by any number between   2 and n  it is not prime
             if (num % i) == 0:
-                return False
+                break
         else:
-            return True
-    else:
-        return False
+            return num
 
 
 def parse_agrs():
     """
     This function is used to parse the user input using by using the argparse
-    :return:
+    :return: A tuple with the start and end
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-s')
@@ -42,8 +61,14 @@ def parse_agrs():
 
 
 def primes(start, end):
+    """
+    This function calculates the prime numbers in a given range and prints it out
+    :param start: range start
+    :param end: range end
+    :return: NA
+    """
     lst = []
-    for ix in range(start, end):
+    for ix in range(start, end + 1):
         if isprime(ix):
             lst.append(ix)
 
@@ -54,4 +79,3 @@ def primes(start, end):
 if __name__ == "__main__":
     arg = parse_agrs()
     primes(arg[0], arg[1])
-
