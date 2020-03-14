8be474db4f7546eb151112398e8c9f9dab42ca12
Rakshith Venkatachalapathy <54309883+rakshithvenkatachalapathy@users.noreply.github.com>
2020-03-14 04:17:40 -0700

    request_procreation

--- Assignments/Midterm_Project/dolphin_pop_model.py
+++ Assignments/Midterm_Project/dolphin_pop_model.py
@@ -52,6 +52,34 @@ def yieldName(gender):
     f.close()
 
 
+class Dolphins:
+    def __init__(self, name, sex, age, mother, father, years_since_procreation, death):
+        self.name = name
+        self.sex = sex
+        self.age = 0
+        self.mother = mother
+        self.father = father
+        self.yeasrs_since_procreation = years_since_procreation
+        self.death = death
+
+    def request_procreation(self, req):
+        flag = True
+        if str(self.sex).casefold() == str(req.sex).casefold():
+            flag = False
+        # Check for age
+        elif abs(self.age - req.age) > 10:
+            flag = False
+        # Parent check
+        elif str(self.mother).casefold() == str(req.name).casefold() or str(self.father).casefold() == str(
+                req.name).casefold():
+            flag = False
+        # Sibling check
+        elif str(self.mother).casefold() == str(req.mother).casefold() or str(self.father).casefold() == str(
+                req.father).casefold():
+            flag = False
+        return flag
+
+
 if __name__ == "__main__":
     mname = yieldName('male')
     print(next(mname))
@@ -59,4 +87,8 @@ if __name__ == "__main__":
     print(next(fname))
     print(next(mname))
     print(next(fname))
+
+    x = Dolphins("rak", "M", 0, "abc", "xyz", 3, 35)
+    y = Dolphins("swa", "F", 2, "abbc", "xvyz", 3, 35)
+    print(x.request_procreation(y))
 
