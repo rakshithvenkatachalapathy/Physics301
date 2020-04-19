f0dfe619eea5b412cfbe5abd4e01731bb5984658
Rakshith Venkatachalapathy <54309883+rakshithvenkatachalapathy@users.noreply.github.com>
2020-04-18 21:22:21 -0700

    extract_shape

---
+++ Assignments/HW06/FourierDescriptor.py
@@ -0,0 +1,47 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def extract_shape(im_file, blowup=1., plot_img=False, plot_contour=False, plot_contour_pts=False):
+    im = mpimg.imread(im_file)
+
+    if len(im.shape) > 2:
+        im = im[:, :, 0]
+
+    if plot_img:
+        plt.figure()
+        plt.title('Original Shape')
+        plt.imshow(im, cmap=plt.cm.gray)
+
+    x = np.arange(im.shape[1]) * blowup
+    y = np.arange(im.shape[0]) * blowup
+
+    y = y[::-1]
+
+    X, Y = np.meshgrid(x, y)
+
+    plt.figure()
+    plt.title('Contours')
+
+    CS = plt.contour(X, Y, im, 1)
+    if not plot_contour:
+        plt.close()
+
+    cs_paths = CS.collections[0].get_paths()
+
+    x_arr = []
+    y_arr = []
+    for i in range(len(cs_paths)):
+        p = cs_paths[i]
+        v = p.vertices
+        x_arr.append(v[:, 0])
+        y_arr.append(v[:, 1])
+
+    if plot_contour_pts:
+        plt.figure()
+        plt.title("Verify the contour points are correct")
+        for i in range(len(x_arr)):
+            plt.scatter(x_arr[i], y_arr[i])
+
+    return x_arr, y_arr
