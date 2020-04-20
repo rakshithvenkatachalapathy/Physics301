38692630f328d450691fb2b932b56f295bb4748f
Rakshith Venkatachalapathy <54309883+rakshithvenkatachalapathy@users.noreply.github.com>
2020-04-19 20:46:30 -0700

    FD functions

--- Assignments/HW06/FourierDescriptor.py
+++ Assignments/HW06/FourierDescriptor.py
@@ -18,7 +18,6 @@ def extract_shape(im_file, blowup=1., plot_img=False, plot_contour=False, plot_c
     y = np.arange(im.shape[0]) * blowup
 
     y = y[::-1]
-
     X, Y = np.meshgrid(x, y)
 
     plt.figure()
@@ -27,9 +26,7 @@ def extract_shape(im_file, blowup=1., plot_img=False, plot_contour=False, plot_c
     CS = plt.contour(X, Y, im, 1)
     if not plot_contour:
         plt.close()
-
     cs_paths = CS.collections[0].get_paths()
-
     x_arr = []
     y_arr = []
     for i in range(len(cs_paths)):
@@ -45,3 +42,31 @@ def extract_shape(im_file, blowup=1., plot_img=False, plot_contour=False, plot_c
             plt.scatter(x_arr[i], y_arr[i])
 
     return x_arr, y_arr
+
+
+def FD(x, y, plot_FD=False, y_lim=None):
+    N = len(x)
+    n = np.arange(N)
+    z = x + y * 1j
+    Z = np.fft.fft(z)
+    if plot_FD is True:
+        plt.figure()
+        plt.title('FD real and imag')
+        plt.plot(Z.real, 'b-')
+        plt.plot(Z.imag, 'g-')
+        if y_lim is not None:
+            plt.ylim([-y_lim, y_lim])
+    return Z
+
+
+def filt_FD(Z, n_keep, no_zeroth=True):
+    N = len(Z)
+    n = np.arange(len(Z))
+    zeroth = 1
+    if no_zeroth:
+        zeroth = n > 0
+    filt1 = zeroth * (n <= n_keep)
+    filt2 = (n > ((N - 1) - n_keep))
+    filt = filt1 + filt2
+    Z *= filt
+    return Z
